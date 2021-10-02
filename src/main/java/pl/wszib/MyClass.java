9e93670e6dbe129163161dc6bef1862285eaf6d3
dotyrala <dotyrala@student.wszib.edu.pl>
2021-10-02 19:03:56 +0200

    boxing unboxing

--- src/main/java/pl/wszib/MyClass.java
+++ src/main/java/pl/wszib/MyClass.java
@@ -1,15 +1,20 @@
 package pl.wszib;
 
+import java.math.BigInteger;
+
 public class MyClass extends MyAbstractClass implements MyInterface, MyInterface2 {
     int myInt;
     Integer myInteger;
+    Long longus;
+    long longer;
+    BigInteger myBigInt;
 
     @Override
     public void doCalculation() {
 
         //autoboxing
         Integer myLocalInt = myInt;
-
+        myBigInt = new BigInteger("111111111111111111111111111111111111111111111111111111");
         //auto unboxing
         int myLocalInte = myInteger;
     }
