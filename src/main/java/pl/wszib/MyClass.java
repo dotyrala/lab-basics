b747222a45eb924b3cbd52ecb09dbf9139bf88ac
dotyrala <dotyrala@student.wszib.edu.pl>
2021-10-02 18:59:18 +0200

    boxing unboxing

--- src/main/java/pl/wszib/MyClass.java
+++ src/main/java/pl/wszib/MyClass.java
@@ -1,9 +1,17 @@
 package pl.wszib;
 
 public class MyClass extends MyAbstractClass implements MyInterface, MyInterface2 {
+    int myInt;
+    Integer myInteger;
+
     @Override
     public void doCalculation() {
 
+        //autoboxing
+        Integer myLocalInt = myInt;
+
+        //auto unboxing
+        int myLocalInte = myInteger;
     }
 
     @Override
